7b1b3a1f321369b457f9aaa3cb73c8101101260d
William <37138564+spyrux@users.noreply.github.com>
2025-09-19 18:57:24 -0400

    Time: 596 ms (42.21%), Space: 20.7 MB (36.16%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                break
-            if i == 0 or nums[i - 1] != nums[i]:
-                self.twoSumII(nums, i, res)
-        return res
+        result = []
+        n = len(nums)
 
-    def twoSumII(self, nums: List[int], i: int, res: List[List[int]]):
-        lo, hi = i + 1, len(nums) - 1
-        while lo < hi:
-            sum = nums[i] + nums[lo] + nums[hi]
-            if sum < 0:
-                lo += 1
-            elif sum > 0:
-                hi -= 1
-            else:
-                res.append([nums[i], nums[lo], nums[hi]])
-                lo += 1
-                hi -= 1
-                while lo < hi and nums[lo] == nums[lo - 1]:
-                    lo += 1
+        for i in range(n - 2):
+            # Skip duplicate i values
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, n - 1
+            while left < right:
+                s = nums[i] + nums[left] + nums[right]
+                if s == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    # Skip duplicate lefts
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    # Skip duplicate rights
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+
+                elif s < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return result
