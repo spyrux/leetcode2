a57d37083b56cedac8d2133f5cff969c697fabef
William <37138564+spyrux@users.noreply.github.com>
2025-09-19 18:55:46 -0400

    Time: 423 ms (93.85%), Space: 20.7 MB (36.16%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,23 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        #sort so we can use two pointers
-        #sliding window does not work on this 
-        #need to use two pointers in a for loop
+        res = []
         nums.sort()
-        result = []
-        unique = set()
-        n = len(nums)
-        for i in range(n-2):
-            left, right = i+1, n-1
-            while left < right:
-                if nums[i]+ nums[left]+nums[right] == 0:
-                    unique.add((nums[i],nums[left],nums[right]))
-                    left+=1
-                    right-=1
-                elif nums[i]+ nums[left]+nums[right] < 0:
-                    left += 1
-                else:
-                    right-=1
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i - 1] != nums[i]:
+                self.twoSumII(nums, i, res)
+        return res
 
-
-        return list(unique)
+    def twoSumII(self, nums: List[int], i: int, res: List[List[int]]):
+        lo, hi = i + 1, len(nums) - 1
+        while lo < hi:
+            sum = nums[i] + nums[lo] + nums[hi]
+            if sum < 0:
+                lo += 1
+            elif sum > 0:
+                hi -= 1
+            else:
+                res.append([nums[i], nums[lo], nums[hi]])
+                lo += 1
+                hi -= 1
+                while lo < hi and nums[lo] == nums[lo - 1]:
+                    lo += 1
