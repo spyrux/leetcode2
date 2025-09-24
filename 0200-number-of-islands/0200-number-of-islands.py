0d29b0045eeeecad8a54f82df999ca91644966ef
William <37138564+spyrux@users.noreply.github.com>
2025-09-24 14:33:16 -0400

    Time: 231 ms (88.34%), Space: 20.1 MB (82.62%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        #go thru grid and find ones to dfs, mark seen for all encountered in dfs
+        width = len(grid[0])
+        length = len(grid)
+        seen = [[False for i in range(width)] for j in range(length)]
+        num = 0
+
+        def dfs(grid: List[List[str]], seen: List[List[str]], y, x):
+            if y < 0 or y >= length or x >= width or x < 0 or seen[y][x] or grid[y][x] == '0':
+                return
+            seen[y][x] = True
+            dfs(grid, seen, y+1,x)
+            dfs(grid, seen, y-1,x)
+            dfs(grid, seen, y,x+1)
+            dfs(grid, seen, y,x-1)
+
+        for i in range(0, length):
+            for j in range(0, width):
+                if grid[i][j] == '1' and seen[i][j] == False:
+                    dfs(grid, seen, i, j)
+                    num += 1
+        return num
+
