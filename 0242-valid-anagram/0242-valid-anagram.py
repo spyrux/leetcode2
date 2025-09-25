3f6e7ffd207a809cab632fd116ae6245d8e1f4e5
William <37138564+spyrux@users.noreply.github.com>
2025-09-25 13:01:20 -0400

    Time: 12 ms (53.06%), Space: 18 MB (34.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,24 @@
 from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        count1 = defaultdict(int)
-        count2 = defaultdict(int)
+        counts = defaultdict(int)
+        if len(s) >= len(t):
+            for i in range(len(s)):
+                counts[s[i]]+=1
+            for i in range(len(t)):
+                counts[t[i]]-=1
+                if counts[t[i]]==0:
+                    counts.pop(t[i])
+        else:
+            for i in range(len(t)):
+                counts[t[i]]+=1
+            for i in range(len(s)):
+                counts[s[i]]-=1
+                if counts[s[i]]==0:
+                    counts.pop(s[i]) 
 
-        for i in range(len(s)):
-            count1[s[i]]+=1
-
-        for i in range(len(t)):
-            count2[t[i]]+=1
+        print(counts)
 
         
-        return count1 == count2
+
+        return not len(counts)>0
