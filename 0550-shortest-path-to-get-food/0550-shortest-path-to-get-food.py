459ed537c03f264befeb9d565e7f6d7e89f45c8a
William <37138564+spyrux@users.noreply.github.com>
2025-09-30 23:48:56 -0400

    Time: 159 ms (6.5%), Space: 27.2 MB (14.13%) - LeetHub

---
+++ 0550-shortest-path-to-get-food/0550-shortest-path-to-get-food.py
@@ -0,0 +1,29 @@
+class Solution:
+    def getFood(self, grid: List[List[str]]) -> int:
+        visited = set()
+        queue = []
+        start = (-1, -1)
+        height = len(grid)
+        width = len(grid[0])
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[0])):
+                if grid[i][j] == "*":
+                    start = (i,j)
+                if grid[i][j] == "#":
+                    queue.append((i, j, 0))
+                    visited.add((i,j))
+        
+        dirs = [(0,1), (0,-1), (1,0), (-1,0)]
+        while queue:
+            y, x, steps = queue.pop(0)
+            for dy, dx in dirs:
+                if 0 <= y+dy < height and 0 <= x+dx < width and (y+dy, x+dx) not in visited and grid[y+dy][x+dx] != "X":
+                    if grid[y+dy][x+dx] == "*":
+                        return steps+1
+                    else:
+                        queue.append((y+dy,x+dx, steps+1))
+                        visited.add((y+dy, x+dx))
+        
+
+        return -1
+            
