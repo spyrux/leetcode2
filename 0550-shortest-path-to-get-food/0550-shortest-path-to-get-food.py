b39992c842a3117572dc8d18646e8c13b2b45542
William <37138564+spyrux@users.noreply.github.com>
2025-09-30 23:58:30 -0400

    Time: 147 ms (8.52%), Space: 27 MB (14.13%) - LeetHub

--- 0550-shortest-path-to-get-food/0550-shortest-path-to-get-food.py
+++ 0550-shortest-path-to-get-food/0550-shortest-path-to-get-food.py
@@ -1,7 +1,9 @@
+from collections import deque
+# use deque for o(1) pop left because its a doubly linked list with o(1)
 class Solution:
     def getFood(self, grid: List[List[str]]) -> int:
         visited = set()
-        queue = []
+        queue = deque()
         start = (-1, -1)
         height = len(grid)
         width = len(grid[0])
@@ -15,7 +17,7 @@ class Solution:
         
         dirs = [(0,1), (0,-1), (1,0), (-1,0)]
         while queue:
-            y, x, steps = queue.pop(0)
+            y, x, steps = queue.popleft()
             for dy, dx in dirs:
                 if 0 <= y+dy < height and 0 <= x+dx < width and (y+dy, x+dx) not in visited and grid[y+dy][x+dx] != "X":
                     if grid[y+dy][x+dx] == "*":
